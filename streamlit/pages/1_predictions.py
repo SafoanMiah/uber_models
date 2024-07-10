c25218236f08aa1bdda9a38b68261575cc9f2d49
saf <saf.home.2017@gmail.com>
2024-07-10 01:01:59 +0100

    Map implementation

--- streamlit/pages/1_predictions.py
+++ streamlit/pages/1_predictions.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 from geopy.geocoders import Nominatim
 
 import os, sys
@@ -13,8 +15,8 @@ st.markdown('---')
 
 
 
-pickup_address, pickup_lat, pickup_long = None, None, None
-dropoff_address, dropoff_lat, dropoff_long = None, None, None
+pickup_address, pickup_lat, pickup_long = None, 0.1276, 51.5072,  #london
+dropoff_address, dropoff_lat, dropoff_long = None, 119.4179, 36.7783 #california
 
 
 #using adress
@@ -33,23 +35,23 @@ st.markdown("<p style='text-align: center;'> OR </p>", unsafe_allow_html=True)
 
 
 
-
+#with latitude and longitude
 if pickup_address:
     getPLoc = loc.geocode(pickup_address)
     pickup_lat, pickup_long = getPLoc.latitude, getPLoc.longitude
     
     col1_1, col1_2= st.columns(2)
     with col1_1:
-        pickup_lat = st.text_input('Pickup Latitude', value=pickup_lat)
+        pickup_lat = st.number_input('Pickup Latitude', value=pickup_lat)
     with col1_2:
-        pickup_long = st.text_input('Pickup Longitude', value=pickup_long)
+        pickup_long = st.number_input('Pickup Longitude', value=pickup_long)
 else:
     #using lat long
     col1_1, col1_2= st.columns(2)
     with col1_1:
-        pickup_lat = st.text_input('Pickup Latitude')
+        pickup_lat = st.number_input('Pickup Latitude')
     with col1_2:
-        pickup_long = st.text_input('Pickup Longitude')
+        pickup_long = st.number_input('Pickup Longitude')
 
 
 if dropoff_address:
@@ -58,12 +60,24 @@ if dropoff_address:
     
     col1_1, col1_2= st.columns(2)
     with col1_1:
-        pickup_lat = st.text_input('Dropoff Latitude', value=pickup_lat)
+        pickup_lat = st.number_input('Dropoff Latitude', value=pickup_lat)
     with col1_2:
-        pickup_long = st.text_input('Dropoff Longitude', value=pickup_long)
+        pickup_long = st.number_input('Dropoff Longitude', value=pickup_long)
 else:
     col2_1, col2_2= st.columns(2)
     with col2_1:
-        dropoff_lat = st.text_input('Dropoff Latitude')
+        dropoff_lat = st.number_input('Dropoff Latitude')
     with col2_2:
-        dropoff_long = st.text_input('Dropoff Longitude')
+        dropoff_long = st.number_input('Dropoff Longitude')
+
+
+
+
+#mapping
+locations = pd.DataFrame({
+    'type': ['green', 'red'],
+    'lat' : [float(pickup_lat), float(dropoff_lat)],
+    'long': [float(pickup_long), float(dropoff_long)]
+})
+
+st.map(locations, latitude='lat', longitude='long', size=5, color='#a6f1a6')
