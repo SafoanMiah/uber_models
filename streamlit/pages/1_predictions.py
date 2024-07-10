f28398b2c36985624c7d4899c1aeb2f785e83a1d
saf <saf.home.2017@gmail.com>
2024-07-10 04:11:41 +0100

    Dataframeimplemenation

--- streamlit/pages/1_predictions.py
+++ streamlit/pages/1_predictions.py
@@ -1,22 +1,24 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle
 from geopy.geocoders import Nominatim
+from datetime import datetime
 
-import os, sys
-sys.path.append(os.path.join(os.getcwd(), '..', 'scripts'))
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'scripts')))
 import distance_utils
 
 
 #title / description
-st.markdown("<h2 style='text-align: center;'> Price Prediction Model </h2>", unsafe_allow_html=True)
-
+st.markdown("<h1 style='text-align: center;'> Price Prediction Model </h1>", unsafe_allow_html=True)
 st.markdown('---')
 
 
 
-pickup_address, pickup_lat, pickup_long = None, 0.1276, 51.5072,  #london
-dropoff_address, dropoff_lat, dropoff_long = None, 119.4179, 36.7783 #california
+pickup_address = 'London'  #london
+dropoff_address = 'California' #california
 
 
 #using adress
@@ -24,10 +26,10 @@ loc = Nominatim(user_agent="Geopy Library")
 
 col1, col2= st.columns(2)
 with col1:
-    pickup_address = st.text_input('Pickup Address')
+    pickup_address = st.text_input('Pickup Address', value='London')
     
 with col2:
-    dropoff_address = st.text_input('Dropoff Address')
+    dropoff_address = st.text_input('Dropoff Address', value='California')
 
 st.markdown("<p style='text-align: center;'> OR </p>", unsafe_allow_html=True)
 
@@ -35,41 +37,42 @@ st.markdown("<p style='text-align: center;'> OR </p>", unsafe_allow_html=True)
 
 
 
-#with latitude and longitude
-if pickup_address:
-    getPLoc = loc.geocode(pickup_address)
-    pickup_lat, pickup_long = getPLoc.latitude, getPLoc.longitude
-    
-    col1_1, col1_2= st.columns(2)
-    with col1_1:
-        pickup_lat = st.number_input('Pickup Latitude', value=pickup_lat)
-    with col1_2:
-        pickup_long = st.number_input('Pickup Longitude', value=pickup_long)
-else:
-    #using lat long
-    col1_1, col1_2= st.columns(2)
-    with col1_1:
-        pickup_lat = st.number_input('Pickup Latitude')
-    with col1_2:
-        pickup_long = st.number_input('Pickup Longitude')
+
+getPLoc = loc.geocode(pickup_address)
+pickup_lat, pickup_long = getPLoc.latitude, getPLoc.longitude
+
+col1_1, col1_2= st.columns(2)
+with col1_1:
+    pickup_lat = st.number_input('Pickup Latitude', value=pickup_lat)
+with col1_2:
+    pickup_long = st.number_input('Pickup Longitude', value=pickup_long)
 
 
-if dropoff_address:
-    getDLoc = loc.geocode(dropoff_address)
-    dropoff_lat, dropoff_long = getDLoc.latitude, getDLoc.longitude
-    
-    col1_1, col1_2= st.columns(2)
-    with col1_1:
-        pickup_lat = st.number_input('Dropoff Latitude', value=pickup_lat)
-    with col1_2:
-        pickup_long = st.number_input('Dropoff Longitude', value=pickup_long)
-else:
-    col2_1, col2_2= st.columns(2)
-    with col2_1:
-        dropoff_lat = st.number_input('Dropoff Latitude')
-    with col2_2:
-        dropoff_long = st.number_input('Dropoff Longitude')
+getDLoc = loc.geocode(dropoff_address)
+dropoff_lat, dropoff_long = getDLoc.latitude, getDLoc.longitude
 
+col1_1, col1_2= st.columns(2)
+with col1_1:
+    pickup_lat = st.number_input('Dropoff Latitude', value=pickup_lat)
+with col1_2:
+    pickup_long = st.number_input('Dropoff Longitude', value=pickup_long)
+
+
+st.markdown("<p style='text-align: center;'> OPTIONAL </p>", unsafe_allow_html=True)
+#other inputs
+col1, col2= st.columns(2)
+with col1:
+    current_year = datetime.now().year
+    print(current_year)
+    year = st.selectbox('Year', (range(2010, current_year+1)), index=len(range(2010, current_year+1))-1)
+with col2:
+    month = st.selectbox('Month', (range(1, 13)))
+
+col1, col2= st.columns(2)
+with col1:
+    day = st.selectbox('Day', (range(1, 8)))
+with col2:
+    hour = st.selectbox('Hour', (range(0, 24)))
 
 
 
@@ -80,4 +83,30 @@ locations = pd.DataFrame({
     'long': [float(pickup_long), float(dropoff_long)]
 })
 
-st.map(locations, latitude='lat', longitude='long', size=5, color='#a6f1a6')
+st.map(locations, latitude='lat', longitude='long', size=10, color='#a6f1a6')
+st.markdown('---')
+
+
+
+
+#model_path = 'uber_cost_prediction/model/trained_model.pkl'
+#with open(model_path, 'rb') as f:
+#    model = pickle.load(f)
+
+distance = distance_utils.haversine(pickup_long, pickup_lat, dropoff_long, dropoff_lat)
+
+st.markdown("<h3 style='text-align: center;'>Daframe:</h3>", unsafe_allow_html=True)
+features = pd.DataFrame({
+    'distance_km': [distance],
+    'pickup_longitude': [pickup_long],
+    'pickup_latitude': [pickup_lat],
+    'dropoff_longitude': [dropoff_long],
+    'dropoff_latitude': [dropoff_lat],
+    'passenger_count': [1],
+    'year': [year],
+    'month': [month],
+    'day': [day],
+    'hour': [hour]
+})
+
+st.dataframe(features)
